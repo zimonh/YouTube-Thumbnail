d1896d670d008ef6a608a160f400bef565573213
Simon <zimonh@hotmail.com>
2018-08-31 01:38:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ License: https://creativecommons.org/licenses/by-nc-sa/4.0/ */
 
 const customThumbnail = inp =>{
 	const qs = e => document.querySelector(e);
-	const qsa = e => document.querySelectorAll(e);
 
 	qs('#'+inp.id).classList.add('aspect-ratio');
 	qs('#'+inp.id).classList.add('customThumbnailPointer');
 
 
 	qs('#'+inp.id).innerHTML = `<iframe  src="" frameborder="0" allowfullscreen></iframe>
-	<img class="coverThumbnail" src="${inp.thumbnail}" >
+	<div class="yt-img-container" style="background-image: url('${inp.thumbnail}');"></div>
 	<div class="coverThumbnail">
 		<div>
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19 13.3"><path class="playIcon" d="M9.6 0C12.1 0 14 0.1 16 0.2c0.7 0 1.4 0.2 1.9 0.7 0.7 0.6 0.9 1.5 0.9 2.3 0.2 2.3 0.2 4.6 0 7 -0.1 0.9-0.3 1.8-1.1 2.4 -0.5 0.4-1 0.5-1.6 0.6 -3.3 0.2-6.6 0.2-9.9 0.1 -1.2 0-2.4 0-3.5-0.2 -1.6-0.2-2.2-0.9-2.5-2.5C0.1 9.7 0 8.9 0 8 0 6.4 0 4.8 0.2 3.2c0.1-0.8 0.2-1.5 0.7-2.1C1.4 0.4 2.2 0.2 3 0.2 5.4 0 7.8 0 9.6 0zM12.7 6.7C11 5.8 9.3 4.9 7.6 4c0 1.8 0 3.5 0 5.3C9.3 8.4 11 7.5 12.7 6.7z"/></svg>
@@ -31,8 +30,9 @@ const customThumbnail = inp =>{
 		}
 
 		setTimeout(()=>{
-			qsa('.coverThumbnail')[0].classList.add('coverThumbnail1');
-			qsa('.coverThumbnail')[1].classList.add('coverThumbnail1');
+
+			qs('.coverThumbnail').classList.add('coverThumbnail1');
+			qs('.yt-img-container').classList.add('coverThumbnail1');
 			qs('#'+inp.id).classList.remove('customThumbnailPointer');
 		}, 500);
 	};
